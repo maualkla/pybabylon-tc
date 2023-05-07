67570c529651fcdab6e5c14b931ea5d0d94e23c9
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-05-07 17:41:05 -0600

    init

---
+++ app.py
@@ -0,0 +1,66 @@
+## Flask Front End app for adminde-tc project.
+## Pybabylon Project.
+## Coded by: Mauricio Alcala (@maualkla)
+## Date: May 2023.
+## More info at @intmau in twitter or in http://maualkla.com
+## Description: Web app to serve adminde-tc project.
+
+
+## Imports
+## Imports
+from flask import Flask, jsonify, request, render_template
+import os 
+
+
+## Initialize Flask App
+app = Flask(__name__)
+
+
+## @TO_BE_DELETED 
+# sample helloworld
+@app.route('/helloworld')
+def hello_world():
+    import requests
+
+    url = 'http://localhost:5000/vlogin'
+    data = {
+        "user": "maualkla",
+        "email": "mauricio@adminde.com",
+        "word": "helloadminde2023"
+        }
+    headers = {'Content-type': 'application/json'}
+
+    response = requests.post(url, json=data, headers=headers)
+    print(response)
+    ##print(response.json())
+    return 'Hello World!'
+
+## @TO_BE_DELETED
+# Landing page
+@app.route('/')
+def landing():
+    return render_template('index.html')
+
+## @TO_BE_DELETED
+# Sample service
+@app.route('/service', methods=['GET'])
+def service():
+    data = request.get_json()
+    if data:
+        return jsonify({
+            "status": "connected",
+            "code": 200
+        })
+    else:
+        return jsonify({
+            "status": "Error",
+            "code": 500
+        })
+    
+## API Status
+@app.route('/status')
+def status():
+    return "Running fine"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=int(os.envirom.get('PORT', 8080)))
