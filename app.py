cdfe209e73bce88e92e848ba8ab27b748776f4db
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-07-02 00:15:43 -0600

    clean_outdated_services

--- app.py
+++ app.py
@@ -26,53 +26,6 @@ def apidocs():
 def apidocs_v0_1():
     return render_template('apidocs_v0_1.html')
 
-## @TO_BE_DELETED 
-# sample helloworld
-@app.route('/postman', methods=['POST'])
-def postman():
-    url = request.json['url']
-    method = request.json['method']
-    key1 = request.json['key1']
-    val1 = request.json['val1']
-    key2 = request.json['key2']
-    val2 = request.json['val2']
-    key3 = request.json['key3']
-    val3 = request.json['val3']
-
-    data = {
-        key1: val1,
-        key2: val2,
-        key3: val3
-        }
-    print(data)
-    headers = {'Content-type': 'application/json'}
-    print("validando")
-    if method == 'post':
-        print("go to post")
-        response = requests.post(url, json=data, headers=headers)
-    else: 
-        print("go to get")
-        response = requests.get(url, headers=headers)
-    
-    print(response.json())
-    return response.json()
-
-## @TO_BE_DELETED 
-# sample helloworld
-@app.route('/helloworld')
-def hello_world():
-    import requests
-
-    url = 'http://localhost:3000/login?u=jp@adminde.com&p=aG9sYTEyMw=='
-    headers = {'Content-type': 'application/json'}
-
-    response = requests.get(url, headers=headers)
-    ##print(response.json())
-    
-    ##print(response.json())
-    return response.json()
-
-## @TO_BE_DELETED
 # Landing page
 @app.route('/')
 def landing():
@@ -304,23 +257,6 @@ def s_signup():
             return _sign
     except Exception as e:
         return {"status": "An error Occurred", "error": e}
-
-
-## @TO_BE_DELETED
-# Sample service
-@app.route('/service', methods=['GET'])
-def service():
-    data = request.get_json()
-    if data:
-        return jsonify({
-            "status": "connected",
-            "code": 200
-        })
-    else:
-        return jsonify({
-            "status": "Error",
-            "code": 500
-        })
     
 ## API Status
 @app.route('/status')
