fca93d4fdd0a672e46ce21d87fb116be44f18cda
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-08-12 12:00:47 -0600

    feat signup changing functionality

--- static/signup.js
+++ static/signup.js
@@ -8,33 +8,25 @@ if(document.getElementById('i_email')) document.getElementById('i_email').addEve
 if(document.getElementById('i_pass')) document.getElementById('i_pass').addEventListener('change', function (){stage_0_inputs_check();});
 if(document.getElementById('i_pass_repeat')) document.getElementById('i_pass_repeat').addEventListener('change', function (){stage_0_inputs_check();});
 // next button trigger
-if(document.getElementById('_next_button')) document.getElementById('_next_button').addEventListener('click', function (){ if (_valid) nextButton(); });
+if(document.getElementById('_next_button')) document.getElementById('_next_button').addEventListener('click', function (){ if (_valid) nextButton(true); });
+if(document.getElementById('_back_button')) document.getElementById('_back_button').addEventListener('click', function (){ if (_valid) nextButton(false); });
 
 
 // _stage 0 check inputs.
 function stage_0_inputs_check(){_butt = document.getElementById('_next_button');if(document.getElementById('i_full_name').value.length > 0 && document.getElementById('i_username').value.length > 0 && document.getElementById('i_email').value.length > 0 && document.getElementById('i_username').value.length > 0 && document.getElementById('i_pass').value.length > 0 && document.getElementById('i_pass_repeat').value.length > 0){_butt.classList.remove("_gray");/*_butt.classList.add("_box_altern");*/_butt.classList.add("_altern");_valid = true;}else{_butt.classList.add("_gray");_butt.classList.remove("_box_altern");_butt.classList.remove("_altern");_valid = false;}}
 
-function nextButton(){
-    console.log(" next button action");
-    switch (_stage){
-        case 0:
-            /// Hide stage 0 show stage 1
-            document.getElementById('_stage_0').classList.add("_hidden");
-            document.getElementById('_stage_1').classList.remove("_hidden");
-            document.getElementById('_back_button').classList.remove('_hidden');
-            _stage = 1;
-            break;
-        case 1:
-            /// Hide stage 1 show stage 2
-            break;
-        case 2:
-            /// Hide stage 2 show stage 3
-            break;
-        case 3:
-            /// Hide stage 3 show stage 4
-            break;
-        default:
-            break;
+function nextButton(_direction){
+    console.log(" next button action direction "+_direction+ " stage: "+_stage);
+    _stages = ["_stage_0", "_stage_1", "_stage_2", "_stage_3"];
+    if(_direction && _stage > -1){
+        document.getElementById(_stages[_stage]).classList.add("_hidden");
+        document.getElementById(_stages[_stage + 1]).classList.remove("_hidden");
+        document.getElementById('_back_button').classList.remove('_hidden');
+        _stage += 1;
+    }else if(_stage > 0){
+        document.getElementById(_stages[_stage - 1]).classList.remove("_hidden");
+        document.getElementById(_stages[_stage]).classList.add("_hidden");
+        if(_stage == 1){document.getElementById('_back_button').classList.add('_hidden');}
+        _stage -= 1;
     }
-         
 }
