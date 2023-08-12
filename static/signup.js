982a4b83f3d952bb5f159b6e38d6463ed29f321f
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-08-12 13:09:15 -0600

    feature signup fixed

--- static/signup.js
+++ static/signup.js
@@ -15,13 +15,14 @@ if(document.getElementById('_back_button')) document.getElementById('_back_butto
 // _stage 0 check inputs.
 function stage_0_inputs_check(){_butt = document.getElementById('_next_button');if(document.getElementById('i_full_name').value.length > 0 && document.getElementById('i_username').value.length > 0 && document.getElementById('i_email').value.length > 0 && document.getElementById('i_username').value.length > 0 && document.getElementById('i_pass').value.length > 0 && document.getElementById('i_pass_repeat').value.length > 0){_butt.classList.remove("_gray");/*_butt.classList.add("_box_altern");*/_butt.classList.add("_altern");_valid = true;}else{_butt.classList.add("_gray");_butt.classList.remove("_box_altern");_butt.classList.remove("_altern");_valid = false;}}
 
+// _nextButton gets a _direction param to set fordward or backwards as direction. 
 function nextButton(_direction){
     console.log(" next button action direction "+_direction+ " stage: "+_stage);
     _stages = ["_stage_0", "_stage_1", "_stage_2", "_stage_3"];
     if(_direction && _stage > -1){
         document.getElementById(_stages[_stage]).classList.add("_hidden");
         document.getElementById(_stages[_stage + 1]).classList.remove("_hidden");
-        document.getElementById('_back_button').classList.remove('_hidden');
+        if(_stage == 0){document.getElementById('_back_button').classList.remove('_hidden');}
         _stage += 1;
     }else if(_stage > 0){
         document.getElementById(_stages[_stage - 1]).classList.remove("_hidden");
