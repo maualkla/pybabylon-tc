1f55fcfd9ea1aac43b9b2e7bd84e54103088f798
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-08-30 18:08:57 -0600

    fix

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     CONF_URL=os.environ.get('ENV_URL', 'http://127.0.0.1')
     CONF_PORT=os.environ.get('ENV_PORT', '3000')
-    CONF_INSTANCE=os.envirom.get('ENV_NAME', 'DEV')
+    CONF_INSTANCE=os.environ.get('ENV_NAME', 'DEV')
