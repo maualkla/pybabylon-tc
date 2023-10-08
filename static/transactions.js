7cf1db967f4d48371b8f73997e3b20347664c09b
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-10-08 15:26:12 +0200

    feature f trx mgr --- adding js

--- static/transactions.js
+++ static/transactions.js
@@ -3,8 +3,17 @@
 */
 // System vars
 
-
+// Service buttons
 if(document.getElementById('_dash_en')) document.getElementById('_dash_en').addEventListener('click', function (){window.location.replace("/dashboard")});
 if(document.getElementById('_dash_es')) document.getElementById('_dash_es').addEventListener('click', function (){ window.location.replace("/dashboard") });
 if(document.getElementById('_logout_en')) document.getElementById('_logout_en').addEventListener('click', function (){window.location.replace("/logout")});
 if(document.getElementById('_logout_es')) document.getElementById('_logout_es').addEventListener('click', function (){ window.location.replace("/logout") });
+
+// mainblock buttons
+if(document.getElementById('_next_button')) document.getElementById('_next_button').addEventListener('click', function (){ _display_search(); });
+if(document.getElementById('_create_button')) document.getElementById('_create_button').addEventListener('click', function (){ window.location.reload(); });
+
+// functions
+function _display_search(){
+
+}
