c05e441534034a73106f093fa4cb70e53ebd78fd
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-12-31 16:56:00 +0100

    done broooo

--- static/workspace.js
+++ static/workspace.js
@@ -10,9 +10,11 @@ if(document.getElementById('_back_dash_es')) document.getElementById('_back_dash
 if(document.getElementById('_close_sesion_en')) document.getElementById('_close_sesion_en').addEventListener('click', function (){ _redirect("logout"); });
 if(document.getElementById('_close_sesion_es')) document.getElementById('_close_sesion_es').addEventListener('click', function (){ _redirect("logout"); });
 
+/* redirects */
+if(document.getElementById("_create_ws")) document.getElementById("_create_ws").addEventListener("click", function (){ _redirect("workspace/new");});
+if(document.getElementById("_no_workspaces")) document.getElementById("_no_workspaces").addEventListener("click", function (){ _redirect("workspace/new");});
 
 /* local functions */
 const _view_workspace = (id) => {
-    console.log(id)
     _redirect("workspace/"+id);
 }
