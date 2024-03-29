121a9d50c7f4ca1d809835373e739c7e0cbec1c7
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2024-03-29 15:19:02 -0600

    feature: added a switch for the view of the tenant user update,

--- static/workspace_users_update.js
+++ static/workspace_users_update.js
@@ -1,3 +1,21 @@
+
 // initializing view elements.
 _display_fbuttons(true);
 _common_system_auto_change_color();
+
+/* JS functions specific for the toggle function */
+
+const toggleSwitch = document.getElementById("switch");
+const toggleStatus = document.getElementById("switch-status");
+
+function onCheckboxToggle() {
+  const isChecked = this.hasAttribute("checked");
+
+  /* 1. Update toggle switch visual state. */
+  this.toggleAttribute("checked");
+
+  /* 2. Update toggle switch status text. */
+  toggleStatus.innerText = isChecked ? "Regular User" : " Admin User";
+}
+
+toggleSwitch.addEventListener("change", onCheckboxToggle);
