b26da1dc819f15545a9b09882782e2b30396d352
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2024-03-29 22:38:32 -0600

    feat: password change added

--- static/workspace_users_manage.js
+++ static/workspace_users_manage.js
@@ -2,6 +2,7 @@
 //  JS for workspace_users_manage
 //
 
+
 // initializing view elements.
 _display_fbuttons(true);
 _common_system_auto_change_color();
@@ -12,3 +13,4 @@ const _view_tenant_user = (tax_id, id) => {
     _display_wheel(true)
     _redirect("workspace/"+tax_id+"/users/"+id);
 }
+
--- static/workspace_users_update.js
+++ static/workspace_users_update.js
@@ -3,7 +3,31 @@
 _display_fbuttons(true);
 _common_system_auto_change_color();
 
-/* JS functions specific for the toggle function */
+
+// required view variables
+let _view = 0;
+
+
+/// triggers: 
+if(document.getElementById('_input_pass')) document.getElementById('_input_pass').addEventListener('click', function (){ if(_view === 0) { _ws_users_update_display_passview(true); } });
+// floating buttons 
+if(document.getElementById('_fb_1')) document.getElementById('_fb_1').addEventListener('click', function (){ 
+    if(_view === 0) { 
+        // something here 
+    } else if(_view === 1){
+        // something else here
+    }
+});
+if(document.getElementById('_fb_2')) document.getElementById('_fb_2').addEventListener('click', function (){ 
+    if(_view === 0) { 
+        // something here 
+    } else if(_view === 1){
+        _ws_users_update_display_passview(false);
+    }
+});
+
+
+/* JS functions specific for the switch function */
 
 const toggleSwitch = document.getElementById("switch");
 const toggleStatus = document.getElementById("switch-status");
@@ -19,3 +43,18 @@ function onCheckboxToggle() {
 }
 
 toggleSwitch.addEventListener("change", onCheckboxToggle);
+/* end of switch function */
+
+
+// display pass view
+const _ws_users_update_display_passview = ( action ) => {
+    if (action){
+        document.getElementsByClassName("_main_block_content")[0].classList.add("_hidden");
+        document.getElementsByClassName("_main_block_content_pwd")[0].classList.remove("_hidden");
+        _view = 1;
+    }else{
+        document.getElementsByClassName("_main_block_content_pwd")[0].classList.add("_hidden");
+        document.getElementsByClassName("_main_block_content")[0].classList.remove("_hidden");
+        _view = 0;
+    }
+}
